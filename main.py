84e16f7d8954794f85b4dcb485f85c8bfa9bd986
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-11-28 20:21:28 +0200

    fix

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from playwright.sync_api import sync_playwright
+import sys
 
 
 def main():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch()
         page = browser.new_page()
-        page.on('pageerror', lambda exception: print(f'uncaught exception: {exception}'))
+        page.on('pageerror', lambda exception: sys.stdout.write(f'{exception}\n'))
         page.goto('file:///work/docs/index.html')
         page.click('#start-pause-button')
         page.click('#start-pause-button')
